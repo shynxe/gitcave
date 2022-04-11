bd54096e3319dbeb5f277b1487c09ae722edbecc
Armand <shynxe@gmail.com>
2022-04-11 14:17:39 +0300

    Fixed DevTools

--- src/app/store.js
+++ src/app/store.js
@@ -1,16 +1,17 @@
-import {createStore} from '@reduxjs/toolkit';
+import {configureStore} from '@reduxjs/toolkit';
 import storage from 'redux-persist/lib/storage';
 import rootReducer from "../slices/rootReducer";
-import persistReducer from "redux-persist/es/persistReducer";
-import persistStore from "redux-persist/es/persistStore";
-
+import {persistReducer, persistStore} from "redux-persist";
 
 
 const persistConfig = {
-    key: 'counter',
-    storage,
+    key: 'root',
+    version: 1,
+    storage: storage,
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
-export const store = createStore(persistedReducer);
+export const store = configureStore({
+    reducer: persistedReducer,
+});
 export const persistor = persistStore(store);
