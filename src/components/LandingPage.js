37f9ec332abbd6449a8b89dbf1161cfa48b64bf8
alescandruu <alex.daniliuc14@yahoo.com>
2022-04-13 17:29:00 +0300

    Text & Input transition

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./LandingPage.css";
 import pcWork from "../images/pcWork.png";
 import GitHubIcon from "@mui/icons-material/GitHub";
@@ -10,6 +10,7 @@ import {
    rightDivVariants,
    imageVariants,
    welcomeVariants,
+   inputVariants,
 } from "./utils";
 
 function LandingPage() {
@@ -18,6 +19,7 @@ function LandingPage() {
    const handleFirstClick = () => {
       setSecondTransition(true);
    };
+
 
    return (
       <div className="landingPage">
@@ -28,43 +30,56 @@ function LandingPage() {
             variants={leftDivVariants}
             transition={{ duration: "1" }}
          >
-            <h1
+            <motion.h1
                className="welcome"
                initial={"initial"}
-               animate={secondTransition && 'hidden'}
+               animate={secondTransition && "hidden"}
                variants={welcomeVariants}
-               transition={{ duration: "1" }}
+               transition={{delay: '1', duration: "1" }}
             >
                Welcome to
-            </h1>
-            <h1 className="gitCave">
+            </motion.h1>
+            <motion.h1
+               className="gitCave"
+               initial={"initial"}
+               animate={secondTransition && "hidden"}
+               variants={welcomeVariants}
+               transition={{delay: '1', duration: "1" }}
+            >
                <span className="git">Git</span>Cave
-            </h1>
+            </motion.h1>
             <GitHubIcon
                sx={{
                   color: "rgba(166, 218, 255, 1)",
                   marginTop: "12%",
                   mixBlendMode: "color-dodge",
                   fontSize: "7vw",
-                  stroke: "black",
-                  strokeWidth: "0.4",
+                  // stroke: "black",
+                  // strokeWidth: "0.4",
                }}
             />
-            <TextField
-               id="outlined-basic"
-               label="Username"
-               variant="outlined"
-               sx={{
-                  "& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
-                     borderColor: "rgba(166, 218, 255, 1)",
-                     borderWidth: "4px",
-                     borderRadius: "25px",
-                     color: "white",
-                  },
-                  width: "50%",
-                  display: "none",
-               }}
-            />
+            <motion.div
+               initial={"initial"}
+               style={{width: '50%'}}
+               animate={secondTransition && "displayed"}
+               variants={inputVariants}
+               transition={{delay: '1.5', duration: "1" }}
+            >
+               <TextField
+                  id="outlined-basic"
+                  label="Username"
+                  variant="outlined"
+                  sx={{
+                     "& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
+                        borderColor: "rgba(166, 218, 255, 1)",
+                        borderWidth: "4px",
+                        borderRadius: "25px",
+                        color: "white",
+                     },
+                     width: "100%",
+                  }}
+               />
+            </motion.div>
             <Button
                onClick={!secondTransition && handleFirstClick}
                variant="contained"
@@ -74,14 +89,13 @@ function LandingPage() {
                   backgroundColor: "rgba(166, 218, 255, 1)",
                   mixBlendMode: "color-dodge",
                   borderRadius: "50px",
-                  border: "2px solid black",
                   fontSize: "0.9vw",
                   fontWeight: "bolder",
-                  textShadow:
-                     "-1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000",
+                  // textShadow:
+                  //    "-1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000",
                }}
             >
-               Search User
+               {secondTransition ? 'Search User' : "Start"}
             </Button>
          </motion.div>
          <motion.div
--- src/components/utils.js
+++ src/components/utils.js
@@ -1,35 +1,45 @@
 export const leftDivVariants = {
-    initial: {
-        width: '25%'
-    },
-    equalized: {
-        width: '55%'
-    }
-} 
+   initial: {
+      width: "25%",
+   },
+   equalized: {
+      width: "55%",
+   },
+};
 
 export const rightDivVariants = {
-    initial: {
-        width: '75%'
-    },
-    equalized: {
-        width: '45%'
-    }
-}
+   initial: {
+      width: "75%",
+   },
+   equalized: {
+      width: "45%",
+   },
+};
 
 export const imageVariants = {
-    initial: {
-        marginLeft: '25vw'
-    },
-    equalized: {
-        marginLeft: '0%'
-    }
-}
+   initial: {
+      marginLeft: "25vw",
+   },
+   equalized: {
+      marginLeft: "0%",
+   },
+};
 
 export const welcomeVariants = {
-    initial: {
-        opacity: '1'
-    },
-    hidden: {
-        opacity: '0'
-    }
-} 
+   initial: {
+      opacity: 1,
+   },
+   hidden: {
+      opacity: 0,
+   },
+};
+
+export const inputVariants = {
+   initial: {
+      display: "block",
+      opacity: 0
+   },
+   displayed: {
+      opacity: 1
+   },
+};
