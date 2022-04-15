f7581f0887148195346f54c3b3b54ae6e3c3438e
Andrei Chirila <andrei.chirila@evozon.com>
2022-04-15 12:14:21 +0300

    folder structure style

--- src/components/RepositoryItem.js
+++ src/components/RepositoryItem.js
@@ -1,94 +1,93 @@
 import React from "react";
 import Card from "@mui/material/Card";
-import {Box, CardContent, Link, Typography} from "@mui/material";
-import {AiOutlineStar} from "react-icons/ai";
-import {GoRepoForked} from "react-icons/go";
-import {FaBalanceScale, FaCircle} from "react-icons/fa";
+import { Box, CardContent, Link, Typography } from "@mui/material";
+import { AiOutlineStar } from "react-icons/ai";
+import { GoRepoForked } from "react-icons/go";
+import { FaBalanceScale, FaCircle } from "react-icons/fa";
 import moment from "moment";
-import {getColor} from "../utils/utils";
-import {useNavigate} from "react-router-dom";
+import { getColor } from "../utils/utils";
+import { useNavigate } from "react-router-dom";
 
-const RepositoryItem = ({repo}) => {
-    const navigate = useNavigate();
-    const lastUpdated = (date) => {
-        return moment(date).fromNow();
-    };
+const RepositoryItem = ({ repo }) => {
+  const navigate = useNavigate();
+  const lastUpdated = (date) => {
+    return moment(date).fromNow();
+  };
 
-    return (
-        <>
-            <Card sx={{margin: "10px"}}>
-                <CardContent>
-                    <Link
-                        href={""}
-                        sx={{color: "rgba(166, 218, 255, 1)", fontSize: "h5.fontSize"}}
-                        onClick={() =>
-                            navigate(`/browse`,
-                                {
-                                    state: {
-                                        repo: repo,
-                                        current_tree: `https://api.github.com/repos/${repo.owner.login}/${repo.name}/git/trees/${repo.default_branch}`
+  console.log(repo);
 
-                                    },
-                               })
-                        }
-                    >
-                        {repo.name}
-                    </Link>
-                    <Typography
-                        sx={{
-                            display: "inline-block",
-                            padding: "0 7px",
-                            fontSize: "12px",
-                            fontWeight: "500",
-                            lineHeight: "18px",
-                            border: "1px solid grey",
-                            borderRadius: "2em",
-                            color: "#57606a",
-                            margin: "10px",
-                        }}
-                    >
-                        {repo.visibility}
-                    </Typography>
+  return (
+    <>
+      <Card sx={{ margin: "10px" }}>
+        <CardContent>
+          <Link
+            href={""}
+            sx={{ color: "rgba(166, 218, 255, 1)", fontSize: "h5.fontSize" }}
+            onClick={() =>
+              navigate(`/browse`, {
+                state: {
+                  repo: repo,
+                  current_tree: `https://api.github.com/repos/${repo.owner.login}/${repo.name}/git/trees/${repo.default_branch}`,
+                },
+              })
+            }
+          >
+            {repo.name}
+          </Link>
+          <Typography
+            sx={{
+              display: "inline-block",
+              padding: "0 7px",
+              fontSize: "12px",
+              fontWeight: "500",
+              lineHeight: "18px",
+              border: "1px solid grey",
+              borderRadius: "2em",
+              color: "#57606a",
+              margin: "10px",
+            }}
+          >
+            {repo.visibility}
+          </Typography>
 
-                    <Typography sx={{fontSize: 16}}>{repo.description}</Typography>
-                    <Box
-                        sx={{
-                            marginTop: "10px",
-                            display: "flex",
-                            flexDirection: "row",
-                            flexWrap: "wrap",
-                        }}
-                    >
-                        {repo.language ? (
-                            <Typography sx={{marginRight: "15px"}}>
-                                <FaCircle style={{color: getColor(repo.language)}}/> {repo.language}
-                            </Typography>
-                        ) : null}
+          <Typography sx={{ fontSize: 16 }}>{repo.description}</Typography>
+          <Box
+            sx={{
+              marginTop: "10px",
+              display: "flex",
+              flexDirection: "row",
+              flexWrap: "wrap",
+            }}
+          >
+            {repo.language ? (
+              <Typography sx={{ marginRight: "15px" }}>
+                <FaCircle style={{ color: getColor(repo.language) }} />{" "}
+                {repo.language}
+              </Typography>
+            ) : null}
 
-                        <Typography sx={{marginRight: "15px"}}>
-                            <AiOutlineStar/> {repo.stars}
-                        </Typography>
+            <Typography sx={{ marginRight: "15px" }}>
+              <AiOutlineStar /> {repo.stargazers_count}
+            </Typography>
 
-                        <Typography sx={{marginRight: "15px"}}>
-                            <GoRepoForked/> {repo.forks}
-                        </Typography>
+            <Typography sx={{ marginRight: "15px" }}>
+              <GoRepoForked /> {repo.forks}
+            </Typography>
 
-                        {repo.license ? (
-                            <Typography sx={{marginRight: "15px"}}>
-                                <FaBalanceScale/> {repo.license.name}
-                            </Typography>
-                        ) : null}
+            {repo.license ? (
+              <Typography sx={{ marginRight: "15px" }}>
+                <FaBalanceScale /> {repo.license.name}
+              </Typography>
+            ) : null}
 
-                        <Typography sx={{marginRight: "15px"}}>
-
-                            Last Updated: {lastUpdated(repo.updated)}
-                        </Typography>
-                    </Box>
-                </CardContent>
-            </Card>
-        </>
-    )
-        ;
+            <Typography sx={{ marginRight: "15px" }}>
+              Last Updated: {lastUpdated(repo.updated_at)}
+            </Typography>
+          </Box>
+        </CardContent>
+      </Card>
+    </>
+  );
 };
 
 export default RepositoryItem;
--- src/views/FileBrowser.js
+++ src/views/FileBrowser.js
@@ -1,104 +1,132 @@
-import {Link, useLocation, useNavigate} from "react-router-dom";
-import {Box} from "@mui/material";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Grid,
+  Typography,
+  IconButton 
+} from "@mui/material";
 import Navbar from "../components/Navbar";
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import Button from "@mui/material/Button";
-import {useSelector} from "react-redux";
-import {selectUser} from "../slices/userSlice";
+import { useSelector } from "react-redux";
+import { selectUser } from "../slices/userSlice";
+import { AiFillFolder, AiOutlineFile } from "react-icons/ai";
 
 const FileBrowser = () => {
-    const location = useLocation();
-    const navigate = useNavigate();
-    const user = useSelector(selectUser);
-    const [tree, setTree] = useState([]);
+  const location = useLocation();
+  const navigate = useNavigate();
+  const user = useSelector(selectUser);
+  const [tree, setTree] = useState([]);
+  const [dense, setDense] = React.useState(false);
 
-    const {repo, current_tree} = location.state;
-    console.log("current_tree", current_tree);
+  const { repo, current_tree } = location.state;
+  console.log("current_tree", current_tree);
 
-    useEffect(() => {
-        fetch(current_tree)
-            .then(response => response.json())
-            .then(data => {
-                // sort the tree by type and name
-                data.tree.sort((a, b) => {
-                    if (a.type === "tree" && b.type === "tree")
-                        return a.path.localeCompare(b.path);
-                    else if (a.type === "tree")
-                        return -1;
-                    else if (b.type === "tree")
-                        return 1;
-                    else
-                        return a.path.localeCompare(b.path);
-                });
-                setTree(data.tree);
-            });
-    }, [current_tree]);
+  useEffect(() => {
+    fetch(current_tree)
+      .then((response) => response.json())
+      .then((data) => {
+        // sort the tree by type and name
+        data.tree.sort((a, b) => {
+          if (a.type === "tree" && b.type === "tree")
+            return a.path.localeCompare(b.path);
+          else if (a.type === "tree") return -1;
+          else if (b.type === "tree") return 1;
+          else return a.path.localeCompare(b.path);
+        });
+        setTree(data.tree);
+      });
+  }, [current_tree]);
 
-    useEffect(() => {
-        fetch(`${repo.contributors_url}`)
-            .then(response => response.json())
-            .then(data => {
-                console.log(data);
-            });
-    }, [repo]);
+  useEffect(() => {
+    fetch(`${repo.contributors_url}`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      });
+  }, [repo]);
 
-    return (
-        <Box sx={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center"
-        }}>
-            <Box sx={{minHeight: "10vh", width: "95%"}}>
-                <Navbar/>
-            </Box>
-            <Box display="flex" flexGrow="1"
-                 sx={{
-                     flexDirection: {xs: "column", md: "row"},
-                     maxWidth: "70vw",
-                     alignItems: {xs: 'center', md: 'flex-start'}
-                 }}>
-                <Box width={"100%"}>
-                    <ul>
-                        {current_tree.split("/").pop() !== repo.default_branch &&
-                            <div
-                                style={{
-                                    color: "blue",
-                                    cursor: "pointer"
-                                }}
-                                onClick={() => {
-                                    window.history.back();
-                                }}
-                            >
-                                (...)
-                            </div>
-                        }
-                        {tree.map(leaf =>
-                            <li
-                                key={leaf.path}
-                                style={{
-                                    color: leaf.type === "tree" ? "blue" : "black",
-                                    cursor: leaf.type === "tree" ? "pointer" : "default"
-                                }}
-                                onClick={() => {
-                                    console.log(leaf);
-                                    navigate(`/browse`,
-                                        {
-                                            state: {
-                                                repo: repo,
-                                                current_tree: leaf.url,
-                                            },
-                                        })
-                                }}
-                            >
-                                {leaf.path}
-                            </li>
-                        )}
-                    </ul>
-                    <Button component={Link} to={"/user"}>Back to user</Button>
-                </Box>
-            </Box>
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <Box sx={{ minHeight: "10vh", width: "95%" }}>
+        <Navbar />
+      </Box>
+      <Box
+        display="flex"
+        flexGrow="1"
+        sx={{
+          flexDirection: { xs: "column", md: "row" },
+          width: "70vw",
+          margin: "0 auto",
+          alignItems: { xs: "center", md: "flex-start" },
+        }}
+      >
+        <Box width={"100%"}>
+          <Grid container spacing={1}>
+            <Grid item xs={12} md={100}>
+              <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
+                {current_tree.split("/").pop() !== repo.default_branch && (
+                  <div
+                    style={{
+                      color: "blue",
+                      cursor: "pointer",
+                    }}
+                    onClick={() => {
+                      window.history.back();
+                    }}
+                  >
+                    (...)
+                  </div>
+                )}
+              </Typography>
+              <List dense={dense}>
+                {tree.map((leaf) => (
+                  <ListItem key={leaf.path}>
+                    <ListItemIcon>
+                      {leaf.type === "tree" ? (
+                        <AiFillFolder />
+                      ) : (
+                        <AiOutlineFile />
+                      )}
+                    </ListItemIcon>
+                    <ListItemText
+                      primary={leaf.path}
+                      style={{
+                        color: leaf.type === "tree" ? "blue" : "black",
+                        cursor: leaf.type === "tree" ? "pointer" : "default",
+                      }}
+                      onClick={() => {
+                        navigate(`/browse`, {
+                          state: {
+                            repo: repo,
+                            current_tree: leaf.url,
+                          },
+                        });
+                      }}
+                    />
+                  </ListItem>
+                ))}
+              </List>
+            </Grid>
+          </Grid>
+
+          <Button component={Link} to={"/user"}>
+            Back to user
+          </Button>
         </Box>
-    )
+      </Box>
+    </Box>
+  );
 };
 
 export default FileBrowser;
